c2d6b218a3dbe826e5600d76c32b1c398a123290
LuchoPeschiutta <peschiutta.lucho@gmail.com>
2023-02-24 19:07:25 -0300

    Modificada la DB CatalogoPlantas. Modificada estructura, añadidas claves foraneas y restricciones.

--- src/main/java/vivero/FabricaPlantas.java
+++ src/main/java/vivero/FabricaPlantas.java
@@ -28,7 +28,7 @@ public class FabricaPlantas {
     public FabricaPlantas(){
         
         try{
-            connect = DriverManager.getConnection("jdbc:sqlite:./src/main/java/vivero/CatalogoPlantas.s3db");
+            connect = DriverManager.getConnection("jdbc:sqlite:./src/main/java/vivero/CatalogoPlantas.db");
         }catch(SQLException e){
             System.out.println("No se pudo conectar a la DB" + e);
         }
@@ -48,9 +48,11 @@ public class FabricaPlantas {
         
         try {
             PreparedStatement st = connect.prepareStatement("""
-                                                            SELECT P.ID, P.Nombre, P.Tipo AS TipoPlanta, E.Tipo AS TipoEtapa, E.hMax, E.hMin, E.tMax, E.tMin, E.lMax, E.lMin, E.Duracion
-                                                            FROM Plantas P JOIN PlantasEtapas PE ON P.ID=PE.PlantaID  JOIN Etapas E ON PE.EtapaID = E.ID
-                                                            WHERE P.Nombre =\"""" + nombre + "\";");
+                                                            SELECT P.Nombre_Planta AS Nombre, P.Tipo_Planta_ID AS TipoPlanta, E.Tipo_Etapa AS TipoEtapa, E.hMax, E.hMin, E.tMax, E.tMin, E.lMax, E.lMin, E.Duracion 
+                                                            FROM Plantas P JOIN Etapas E ON P.Planta_ID=E.Planta_ID
+                                                            WHERE P.Nombre_Planta = \"""" + nombre + "\""+"""
+                                                            ORDER BY E.Tipo_Etapa ASC
+                                                            """);
             result = st.executeQuery();
             
             if(result.next()){
@@ -106,7 +108,7 @@ public class FabricaPlantas {
         
         try {
             PreparedStatement st = connect.prepareStatement("""
-                                                            SELECT Nombre From TiposPlantas;
+                                                            SELECT Nombre_Tipo AS Nombre From TiposPlanta;
                                                             """);
             result = st.executeQuery();
             
@@ -131,9 +133,9 @@ public class FabricaPlantas {
         
         try {
             PreparedStatement st = connect.prepareStatement("""
-                                                            SELECT P.Nombre FROM Plantas P
-                                                            JOIN TiposPlantas TP ON P.Tipo=TP.ID
-                                                            WHERE TP.Nombre = "Perenne";""");
+                                                            SELECT P.Nombre_Planta AS Nombre FROM Plantas P
+                                                            JOIN TiposPlanta TP ON P.Tipo_Planta_ID=TP.Tipo_Planta_ID
+                                                            WHERE TP.Nombre_Tipo = "Perenne";""");
             result = st.executeQuery();
             
             while(result.next()){
@@ -157,9 +159,9 @@ public class FabricaPlantas {
         
         try {
             PreparedStatement st = connect.prepareStatement("""
-                                                            SELECT P.Nombre FROM Plantas P
-                                                            JOIN TiposPlantas TP ON P.Tipo=TP.ID
-                                                            WHERE TP.Nombre = "NoPerenne";""");
+                                                            SELECT P.Nombre_Planta AS Nombre FROM Plantas P
+                                                            JOIN TiposPlanta TP ON P.Tipo_Planta_ID=TP.Tipo_Planta_ID
+                                                            WHERE TP.Nombre_Tipo = "NoPerenne";""");
             result = st.executeQuery();
             
             while(result.next()){
@@ -176,37 +178,37 @@ public class FabricaPlantas {
 
 /* Creado y llenado de la DB CatalogoPlantas de muestra:
 
-CREATE TABLE [Plantas] (
-[ID] INTEGER  PRIMARY KEY AUTOINCREMENT NOT NULL,
-[Tipo] INTEGER  NOT NULL,
-[Nombre] VARCHAR(50)  NOT NULL
+CREATE TABLE "TiposPlanta" (
+	"Tipo_Planta_ID"    INTEGER NOT NULL,
+	"Nombre_Tipo"       TEXT NOT NULL,
+	PRIMARY KEY("Tipo_Planta_ID")
 );
 
-CREATE TABLE [TiposPlantas] (
-[ID] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-[Nombre] VARCHAR(30)  NOT NULL
+CREATE TABLE "Plantas" (
+	"Planta_ID"         INTEGER NOT NULL,
+	"Tipo_Planta_ID"    INTEGER NOT NULL,
+	"Nombre_Planta"     TEXT NOT NULL,
+	FOREIGN KEY("Tipo_Planta_ID") REFERENCES "TiposPlanta"("Tipo_Planta_ID"),
+	PRIMARY KEY("Planta_ID" AUTOINCREMENT)
 );
 
-CREATE TABLE [Etapas] (
-[ID] INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT,
-[Tipo] INTEGER  NOT NULL,
-[hMax] FLOAT  NOT NULL,
-[hMin] FLOAT  NOT NULL,
-[tMax] FLOAT  NOT NULL,
-[tMin] FLOAT  NOT NULL,
-[lMax] FLOAT  NOT NULL,
-[lMin] FLOAT  NOT NULL,
-[Duracion] INTEGER  NOT NULL
+CREATE TABLE "Etapas" (
+	"Etapa_ID"  INTEGER NOT NULL,
+	"Tipo_Etapa"      INTEGER NOT NULL CHECK("Tipo_Etapa" >= 0 AND "Tipo_Etapa" <= 3),
+	"hMax"      REAL NOT NULL,
+	"hMin"      REAL NOT NULL,
+	"tMax"      REAL NOT NULL,
+	"tMin"      REAL NOT NULL,
+	"lMax"      REAL NOT NULL,
+	"lMin"      REAL NOT NULL,
+	"Duracion"  INTEGER NOT NULL,
+	"Planta_ID" INTEGER NOT NULL,
+	PRIMARY KEY("Etapa_ID" AUTOINCREMENT),
+	FOREIGN KEY("Planta_ID") REFERENCES "Plantas"("Planta_ID")
 );
 
-CREATE TABLE [PlantasEtapas] (
-[PlantaID] INTEGER  NOT NULL,
-[EtapaID] INTEGER  NOT NULL,
-PRIMARY KEY ([PlantaID],[EtapaID])
-);
-
-INSERT INTO Tipos VALUES (NULL, "Perenne");
-INSERT INTO Tipos VALUES (NULL,"NoPerenne");
+INSERT INTO TiposPlanta VALUES (1, "Perenne");
+INSERT INTO TiposPlanta VALUES (2,"NoPerenne");
 
 INSERT INTO Plantas VALUES(NULL, 1, "Pino");
 INSERT INTO Plantas VALUES(NULL, 1, "Rosa");
@@ -218,87 +220,47 @@ INSERT INTO Plantas VALUES(NULL, 2, "Maiz");
 INSERT INTO Plantas VALUES(NULL, 2, "Alfa");
 INSERT INTO Plantas VALUES(NULL, 2, "Mani");
 
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 100, 1);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 100, 1);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 100, 1);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 100, 1);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 100);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 100);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 100);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 100);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 110, 2);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 110, 2);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 110, 2);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 110, 2);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 110);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 110);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 110);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 110);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 120, 3);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 120, 3);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 120, 3);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 120, 3);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 120);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 120);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 120);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 120);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 130, 4);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 130, 4);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 130, 4);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 130, 4);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 130);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 130);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 130);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 130);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 10, 5);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 10, 5);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 10, 5);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 10, 5);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 10);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 10);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 10);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 10);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 15, 6);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 15, 6);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 15, 6);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 15, 6);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 15);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 15);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 15);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 15);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 20, 7);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 20, 7);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 20, 7);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 20, 7);
 
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 20);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 20);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 20);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 20);
-
-INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 25);
-INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 25);
-INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 25);
-INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 25);
+INSERT INTO Etapas VALUES (NULL, 0, 90.0, 10.0, 40.0, 10.5, 60, 10, 25, 8);
+INSERT INTO Etapas VALUES (NULL, 1, 90.0, 15.0, 40.0, 10.5, 60, 10, 25, 8);
+INSERT INTO Etapas VALUES (NULL, 2, 90.0, 20.0, 40.0, 10.5, 60, 10, 25, 8);
+INSERT INTO Etapas VALUES (NULL, 3, 90.0, 25.0, 40.0, 10.5, 60, 10, 25, 8);
 
 
-INSERT INTO PlantasEtapas VALUES (1, 1);
-INSERT INTO PlantasEtapas VALUES (1, 2);
-INSERT INTO PlantasEtapas VALUES (1, 3);
-INSERT INTO PlantasEtapas VALUES (1, 4);
-
-INSERT INTO PlantasEtapas VALUES (2, 5);
-INSERT INTO PlantasEtapas VALUES (2, 6);
-INSERT INTO PlantasEtapas VALUES (2, 7);
-INSERT INTO PlantasEtapas VALUES (2, 8);
-
-INSERT INTO PlantasEtapas VALUES (3, 9);
-INSERT INTO PlantasEtapas VALUES (3, 10);
-INSERT INTO PlantasEtapas VALUES (3, 11);
-INSERT INTO PlantasEtapas VALUES (3, 12);
-
-INSERT INTO PlantasEtapas VALUES (4, 13);
-INSERT INTO PlantasEtapas VALUES (4, 14);
-INSERT INTO PlantasEtapas VALUES (4, 15);
-INSERT INTO PlantasEtapas VALUES (4, 16);
-
-INSERT INTO PlantasEtapas VALUES (5, 17);
-INSERT INTO PlantasEtapas VALUES (5, 18);
-INSERT INTO PlantasEtapas VALUES (5, 19);
-INSERT INTO PlantasEtapas VALUES (5, 20);
-
-INSERT INTO PlantasEtapas VALUES (6, 21);
-INSERT INTO PlantasEtapas VALUES (6, 22);
-INSERT INTO PlantasEtapas VALUES (6, 23);
-INSERT INTO PlantasEtapas VALUES (6, 24);
-
-INSERT INTO PlantasEtapas VALUES (7, 25);
-INSERT INTO PlantasEtapas VALUES (7, 26);
-INSERT INTO PlantasEtapas VALUES (7, 27);
-INSERT INTO PlantasEtapas VALUES (7, 28);
-
-INSERT INTO PlantasEtapas VALUES (8, 29);
-INSERT INTO PlantasEtapas VALUES (8, 30);
-INSERT INTO PlantasEtapas VALUES (8, 31);
-INSERT INTO PlantasEtapas VALUES (8, 32);
 
 */
 
